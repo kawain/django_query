fdf4319b244439006595f0767fe4a90ab0d6ab27
unknown <unknown@example.com>
2021-04-05 19:49:12 +0900

    5

--- test_orm.py
+++ test_orm.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_query.settings")
 
@@ -17,16 +18,74 @@ from blog.models import Category, Tag, Article  # noqa
 #
 
 def f1():
+    """カテゴリ１つ追加"""
+
     obj = Category()
     obj.name = "Python"
     obj.save()
 
 
 def f2():
+    """カテゴリまとめて追加"""
+
+    a = [
+        "Nim",
+        "Rust",
+        "JavaScript",
+        "GO",
+        "Java",
+        "PHP",
+        "C言語",
+        "C++",
+        "C#",
+        "TypeScript",
+        "Perl",
+        "Swift",
+        "Ruby",
+        "R",
+        "Kotlin",
+        "Scala",
+        "FORTRAN",
+        "LISP",
+    ]
+
+    for v in tqdm(a):
+        obj = Category()
+        obj.name = v
+        obj.save()
+
+
+def f3():
+    """カテゴリを全部見る"""
+
     qs = Category.objects.all()
-    print(qs)
-    print(qs[0].id)
-    print(qs[0].name)
+
+    for v in qs:
+        print(v.id)
+        print(v.name)
+        print("-" * 20)
 
 
-f2()
+def f4():
+    """タグを適当に作る"""
+
+    a = [f"tag_{v}" for v in range(1, 11)]
+
+    for v in tqdm(a):
+        obj = Tag()
+        obj.name = v
+        obj.save()
+
+
+def f5():
+    """タグを全部見る"""
+
+    qs = Tag.objects.all()
+
+    for v in qs:
+        print(v.id)
+        print(v.name)
+        print("-" * 20)
+
+
+f5()
